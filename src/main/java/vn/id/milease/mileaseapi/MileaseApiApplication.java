0d6bbc909dd20489219d5f3e5e9de2529407021b
Tran Dinh Khanh <54855318+khanhdinh02@users.noreply.github.com>
2023-09-12 02:08:44 +0700

    initial commit

---
+++ src/main/java/vn/id/milease/mileaseapi/MileaseApiApplication.java
@@ -0,0 +1,13 @@
+package vn.id.milease.mileaseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MileaseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MileaseApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/id/milease/mileaseapi/MileaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.id.milease.mileaseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MileaseApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
